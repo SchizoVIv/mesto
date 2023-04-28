64e915a08e97b1e1993892ad3488e87c5729ce31
Vlada Yakowleva <vladyakowleva@yandex.ru>
2023-04-28 19:52:06 +0300

    fix: реализовала изменение данных профиля

--- js/UserInfo.js
+++ js/UserInfo.js
@@ -2,13 +2,17 @@ export default class UserInfo{
   constructor({userNameSelector, aboutUserSelector, userInputNameSelector, abouInputtUserSelector}){
     this._userName = document.querySelector(userNameSelector);
     this._aboutUser = document.querySelector(aboutUserSelector);
-    this._userInputName = document.querySelector(userInputNameSelector);
-    this._aboutInputUser = document.querySelector(abouInputtUserSelector);
   }
 
-  setUserInfo = () =>{
+  getUserInfo () {
+    return  {
+      userName: this._userName.textContent,
+      aboutUser: this._aboutUser.textContent
+    }
+  }
 
-    this._userName.textContent = this._userInputName.value;
-    this._aboutUser.textContent = this._aboutInputUser.value;
+  setUserInfo = ({userName, aboutUser}) =>{
+    this._userName.textContent = userName;
+    this._aboutUser.textContent = aboutUser;
   }
 }
